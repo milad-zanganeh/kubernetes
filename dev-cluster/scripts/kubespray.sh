ac6e12ab705565df83933212f4ad352993fbe1a8
Milad Zangeneh <miluxhd@gmail.com>
2026-03-23 01:38:03 +0100

    upgrade kube nodes to v1.32.10

--- dev-cluster/scripts/kubespray.sh
+++ dev-cluster/scripts/kubespray.sh
@@ -5,7 +5,6 @@ SSH_PRIVATE_KEY_PATH="${HOME}/.ssh/id_vagrant"
 SSH_PUBLIC_KEY_PATH="${HOME}/.ssh/id_vagrant.pub"
 mkdir -p "$CREDENTIALS_DIR"
 
-source "$SCRIPT_DIR/.env"
 docker run --rm -it \
   --mount type=bind,source="$SCRIPT_DIR"/../inventory,dst=/inventory \
   --mount type=bind,source="$CREDENTIALS_DIR",dst=/inventory/k8s_cluster/credentials \
@@ -13,6 +12,6 @@ docker run --rm -it \
   --mount type=bind,source="$SSH_PRIVATE_KEY_PATH",dst=/root/.ssh/id_vagrant.orig,readonly \
   --network host \
   --mount type=bind,source="${SCRIPT_DIR}"/../scripts/,dst=/scripts \
-  quay.io/kubespray/kubespray:v2.28.0 /scripts/entrypoint.sh
+  quay.io/kubespray/kubespray:v2.30.0 /scripts/entrypoint.sh
 
 
